68a2de04b555378afde663e7248d3d90a4b4d2c6
Irving Orozco <irvingorozco@gmail.com>
2016-07-16 02:05:24 -0400

    Initial Commit

---
+++ Testing.Evolve.Build/Testing.Evolve.Build/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Testing.Evolve.Build.Startup))]
+namespace Testing.Evolve.Build
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
